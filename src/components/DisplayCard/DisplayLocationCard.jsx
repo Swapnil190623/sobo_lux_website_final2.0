b664ef0ae4bc2d1fec645fce0b6beeedfe91c39f
dipeshRR <dipesh.reflexrealty@gmail.com>
2025-02-18 11:23:28 +0530

    added skeleton in all the cards

--- src/components/DisplayCard/DisplayLocationCard.jsx
+++ src/components/DisplayCard/DisplayLocationCard.jsx
@@ -1,10 +1,13 @@
 
 
+
+
+
 // //testing 
 // import React from "react";
 // import { Link } from "react-router-dom";
 // import { motion } from "framer-motion";
-// import { useInView } from "react-intersection-observer"; // Corrected import for useInView
+// import { useInView } from "react-intersection-observer";
 
 // const DisplayLocationCard = ({
 //   title,
@@ -12,56 +15,53 @@
 //   label,
 //   labelColor = "bg-red-500",
 // }) => {
-//   // Assuming 'title' represents the location. Adjust if necessary.
 //   const locationQuery = encodeURIComponent(title);
-
-//   // Correctly use useInView hook from 'react-intersection-observer'
 //   const { ref, inView } = useInView({
-//     triggerOnce: true, // Trigger animation once
-//     threshold: 0.1, // Trigger when 10% of the card is visible
+//     triggerOnce: true,
+//     threshold: 0.1,
 //   });
 
 //   return (
-//     <Link to={`/properties?location=${locationQuery}`}>
+//     <Link to={`/projects?location=${locationQuery}`}>
 //       <motion.article
-//         ref={ref} // Attach the ref to the motion article element
-//         className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-8 pt-40 w-full mx-auto bg-gray-800"
+//         ref={ref}
+//         className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-4 pb-8 pt-40 w-full mx-auto 
+//                    bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg transition-transform duration-300 hover:scale-95"
 //         style={{
 //           maxWidth: "200px",
 //           minWidth: "180px",
 //           minHeight: "300px",
 //         }}
 //         initial={{ opacity: 0, y: 20 }}
-//         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }} // Animate based on visibility
-//         transition={{ duration: 0.6 }} // Slower animation duration (increased from 0.3)
-//         whileHover={{ scale: 0.95, transition: { duration: 0.2 } }} // Faster scale transition on hover
+//         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+//         transition={{ duration: 0.6 }}
 //       >
-//         {/* Optional Label */}
 //         {label && (
 //           <motion.div
-//             className={`absolute top-3 left-3 text-white text-xs font-bold px-2 py-1 rounded-md z-10 antialiased ${labelColor}`}
+//             className={`absolute top-3 left-3 text-white text-xs font-bold px-2 py-1 rounded-md z-10 ${labelColor} shadow-md`}
 //             initial={{ scale: 0 }}
-//             animate={{ scale: inView ? 1 : 0 }} // Animate the label scale based on visibility
-//             transition={{ duration: 0.8 }} // Slower animation for label (increased from 0.4)
+//             animate={{ scale: inView ? 1 : 0 }}
+//             transition={{ duration: 0.8 }}
 //           >
 //             {label}
 //           </motion.div>
 //         )}
+        
 //         <motion.img
 //           src={image}
 //           alt={title}
-//           className="absolute inset-0 h-full w-full object-cover rounded-2xl"
-//           style={{
-//             borderRadius: "inherit",
-//           }}
+//           className="absolute inset-0 h-full w-full object-cover rounded-2xl opacity-80"
+//           style={{ borderRadius: "inherit" }}
 //           whileHover={{ scale: 1.1 }}
-//           transition={{ duration: 0.3 }} // Slightly faster hover effect (decreased from 0.4)
+//           transition={{ duration: 0.3 }}
 //         />
-//         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40 rounded-2xl"></div>
+        
+//         <div className="absolute inset-0 bg-gradient-to-t from-gray-900/80 via-gray-900/40 rounded-2xl"></div>
+        
 //         <motion.h3
-//           className="z-10 text-xl font-bold text-white antialiased"
+//           className="z-10 text-xl font-bold text-white antialiased text-center"
 //           whileHover={{ y: -5 }}
-//           transition={{ duration: 0.3 }} // Faster hover effect for text (decreased from 0.4)
+//           transition={{ duration: 0.3 }}
 //         >
 //           {title}
 //         </motion.h3>
@@ -76,9 +76,7 @@
 
 
 
-
-//testing 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
@@ -90,10 +88,15 @@ const DisplayLocationCard = ({
   labelColor = "bg-red-500",
 }) => {
   const locationQuery = encodeURIComponent(title);
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = image;
+    img.onload = () => setImageLoaded(true);
+  }, [image]);
 
   return (
     <Link to={`/projects?location=${locationQuery}`}>
@@ -110,6 +113,7 @@ const DisplayLocationCard = ({
         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
         transition={{ duration: 0.6 }}
       >
+        {/* Label */}
         {label && (
           <motion.div
             className={`absolute top-3 left-3 text-white text-xs font-bold px-2 py-1 rounded-md z-10 ${labelColor} shadow-md`}
@@ -120,18 +124,28 @@ const DisplayLocationCard = ({
             {label}
           </motion.div>
         )}
-        
-        <motion.img
-          src={image}
-          alt={title}
-          className="absolute inset-0 h-full w-full object-cover rounded-2xl opacity-80"
-          style={{ borderRadius: "inherit" }}
-          whileHover={{ scale: 1.1 }}
-          transition={{ duration: 0.3 }}
-        />
-        
+
+        {/* Skeleton Loader */}
+        {!imageLoaded && (
+          <div className="absolute inset-0 bg-gray-700/50 animate-pulse rounded-2xl"></div>
+        )}
+
+        {/* Image */}
+        {imageLoaded && (
+          <motion.img
+            src={image}
+            alt={title}
+            className="absolute inset-0 h-full w-full object-cover rounded-2xl opacity-80"
+            style={{ borderRadius: "inherit" }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+          />
+        )}
+
+        {/* Gradient Overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900/80 via-gray-900/40 rounded-2xl"></div>
-        
+
+        {/* Title */}
         <motion.h3
           className="z-10 text-xl font-bold text-white antialiased text-center"
           whileHover={{ y: -5 }}
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,3 +1,5 @@
+
+
 // import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 // import { FaWhatsapp } from "react-icons/fa";
@@ -39,7 +41,7 @@
 //   };
 
 //   return (
-//     <div className="max-w-md  py-6 px-3">
+//     <div className="w-[350px] flex-shrink-0  max-w-md py-6 px-3">
 //       <div
 //         className="relative backdrop-blur-xl bg-white/20 border border-white/20 shadow-lg rounded-2xl overflow-hidden cursor-pointer transition-transform duration-300 ease-in-out hover:scale-95"
 //         onClick={handleViewDetails}
@@ -61,7 +63,7 @@
 //           </div>
 //           <div className="absolute top-3 right-3">
 //             <span className="bg-white text-[#000000] text-sm font-bold px-3 py-1 rounded-full border-2">
-//               {property.type}
+//               {property.type.toUpperCase()}
 //             </span>
 //           </div>
 //         </div>
@@ -71,8 +73,8 @@
 //           <p className="text-gray-200 font-semibold text-2xl">
 //             {property.price || "Price On Request"}
 //           </p>
-//           <p className="text-sm font-normal text-gray-300">
-//             {property.address || defaultValue}
+//           <p className="text-md font-normal text-gray-300">
+//             {property.location || defaultValue}
 //           </p>
 //         </div>
 
@@ -81,7 +83,7 @@
 //             <IoBedOutline className="h-5 w-5 text-white/80" />
 //             <div className="flex items-center justify-center space-x-1">
 //               <p className="text-white font-bold text-sm">
-//                 {property.bedrooms || "Ask For"}
+//                 {property.bhk || "Ask For"}
 //               </p>
 //               <span className="font-bold text-white text-sm">BHK</span>
 //             </div>
@@ -93,7 +95,7 @@
 //               alt="Map Pin"
 //               className="h-5 w-5 text-white/80"
 //             />
-//             <p className="text-white font-bold text-sm">
+//             <p className="text-white font-bold text-md">
 //               {property.area || defaultValue}
 //             </p>
 //           </div>
@@ -101,13 +103,13 @@
 
 //         <div className="flex gap-4 p-3 border-t border-white/20 text-center">
 //           <button
-//             className="flex-1 bg-[#ffd586] text-black py-2 rounded-xl hover:bg-black hover:text-white transition-colors duration-200"
+//             className="flex-1 bg-[#ffd586] text-black py-2 rounded-xl hover:bg-black font-bold hover:text-white transition-colors duration-200"
 //             onClick={(e) => {
 //               e.stopPropagation();
 //               handleEnquire(e);
 //             }}
 //           >
-//             Know More
+//             Let's Connect
 //           </button>
 
 //           <button
@@ -129,6 +131,14 @@
 
 // export default ProjectCard;
 
+
+
+
+
+
+
+
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
@@ -142,6 +152,7 @@ const ProjectCard = ({ property }) => {
   const defaultImage =
     "https://i.ibb.co/MhfHdt6/sea-view-appartment-in-mumbai.png";
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isImageLoading, setIsImageLoading] = useState(true); // State to track image loading
 
   const handleViewDetails = () => {
     navigate(`/project/${property.id}`);
@@ -169,8 +180,13 @@ const ProjectCard = ({ property }) => {
     setIsPopupOpen(false);
   };
 
+  // Function to handle image load completion
+  const handleImageLoad = () => {
+    setIsImageLoading(false); // Once image is loaded, set isImageLoading to false
+  };
+
   return (
-    <div className="w-[350px] flex-shrink-0  max-w-md py-6 px-3">
+    <div className="w-[350px] flex-shrink-0 max-w-md py-6 px-3">
       <div
         className="relative backdrop-blur-xl bg-white/20 border border-white/20 shadow-lg rounded-2xl overflow-hidden cursor-pointer transition-transform duration-300 ease-in-out hover:scale-95"
         onClick={handleViewDetails}
@@ -178,13 +194,29 @@ const ProjectCard = ({ property }) => {
         <div
           className="relative bg-cover bg-center h-60 p-4 w-full rounded-sm"
           style={{
-            backgroundImage: `url(${property.image || defaultImage})`,
+            backgroundImage: isImageLoading
+              ? "none" // Show skeleton when image is loading
+              : `url(${property.image || defaultImage})`, // Show image once loaded
             backgroundSize: "cover",
             backgroundPosition: "bottom",
             backgroundRepeat: "no-repeat",
           }}
           onError={handleBrokenImage}
         >
+          {/* Skeleton Loader */}
+          {isImageLoading && (
+            <div className="absolute inset-0 bg-gray-200 animate-pulse" />
+          )}
+
+          {/* Image */}
+          <img
+            src={property.image || defaultImage}
+            alt="Property"
+            className="hidden"
+            onLoad={handleImageLoad} // This triggers when the image is fully loaded
+            onError={handleBrokenImage}
+          />
+
           <div className="absolute top-3 left-3">
             <span className="text-black bg-[#ffd586] text-sm font-bold px-3 py-1 rounded-full">
               {property.location || defaultValue}
@@ -260,132 +292,6 @@ const ProjectCard = ({ property }) => {
 
 export default ProjectCard;
 
-// card with transparent bg ..
 
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { FaWhatsapp } from "react-icons/fa";
-// import { IoBedOutline } from "react-icons/io5"; // Importing IoBedOutline icon
-// import PopupForm from "../PopupForm/PopupForm.jsx"; // Import the PopupForm component
 
-// // Import your SVG as an image
-// import AreaIcon from "./Area.svg"; // Adjust the path according to your file structure
-
-// const ProjectCard = ({ property }) => {
-//   const navigate = useNavigate();
-//   const defaultValue = "Ask For Details";
-//   const defaultImage =
-//     "https://i.ibb.co/MhfHdt6/sea-view-appartment-in-mumbai.png";
-//   const [isPopupOpen, setIsPopupOpen] = useState(false); // State to control the popup
-
-//   const handleViewDetails = () => {
-//     navigate(`/project/${property.id}`);
-//   };
-
-//   const handleBrokenImage = (e) => {
-//     e.target.style.backgroundImage = `url(${defaultImage})`;
-//   };
-
-//   const handleWhatsApp = (e) => {
-//     e.stopPropagation(); // Prevent the click from bubbling to the parent div
-//     const whatsappNumber = "919224448861"; // Replace with actual number
-//     const message = encodeURIComponent(
-//       `Hello, I'm interested in the property: ${property.type}`
-//     );
-//     window.open(`https://wa.me/${whatsappNumber}?text=${message}`, "_blank");
-//   };
-
-//   const handleEnquire = (e) => {
-//     e.stopPropagation(); // Prevent the click from bubbling to the parent div
-//     setIsPopupOpen(true); // Open the popup when the 'Enquire' button is clicked
-//   };
-
-//   const closePopup = () => {
-//     setIsPopupOpen(false); // Close the popup
-//   };
-
-//   return (
-//     <div className="max-w-md w-full py-6 px-3">
-//       <div
-//         className=" backdrop-blur-xl border border-gray-700 shadow-xl rounded-2xl overflow-hidden cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-95"
-//         onClick={handleViewDetails} // Navigate on click
-//       >
-//         <div
-//           className="relative bg-cover bg-center h-60 p-4 w-80 rounded-sm"
-//           style={{
-//             backgroundImage: `url(${property.image || defaultImage})`,
-//             backgroundSize: "cover",
-//             backgroundPosition: "bottom",
-//             backgroundRepeat: "no-repeat",
-//           }}
-//           onError={handleBrokenImage}
-//         >
-//           <div className="absolute top-3 left-3">
-//             <span className="text-white bg-[#4F46E5] text-sm font-bold px-3 py-1 rounded-full">
-//               {property.location || defaultValue}
-//             </span>
-//           </div>
-//           <div className="absolute top-3 right-3">
-//             <span className="bg-white text-[#4F46E5] text-sm font-bold px-3 py-1 rounded-full border-2">
-//               {property.type}
-//             </span>
-//           </div>
-//         </div>
-//         <div className="p-4 text-center font-custom">
-//           <p className="text-gray-300 font-semibold text-2xl">
-//             {property.price || "Price On Request"}
-//           </p>
-//           <p className="text-sm font-normal text-gray-400">
-//             {property.address || defaultValue}
-//           </p>
-//         </div>
-//         <div className="flex p-4 border-t border-gray-600 text-gray-300 text-center font-custom">
-//           {/* Bed section */}
-//           <div className="flex-1 inline-flex items-center justify-center flex-col space-y-1 border border-gray-600 p-2 rounded-2xl mr-4">
-//             <IoBedOutline className="h-5 w-5 text-gray-400" />
-//             <div className="flex items-center justify-center space-x-1">
-//               <p className="text-gray-300 font-bold text-sm">
-//                 {property.bedrooms || "Ask For"}
-//               </p>
-//               <span className="font-bold text-gray-300 text-sm">BHK</span>
-//             </div>
-//           </div>
-
-//           {/* Area section */}
-//           <div className="flex-1 inline-flex items-center justify-center flex-col space-y-1 border border-gray-600 p-2 rounded-2xl">
-//             <img src={AreaIcon} alt="Map Pin" className="h-5 w-5 text-gray-400" />
-//             <p className="text-gray-300 font-bold text-sm">
-//               {property.area || defaultValue}
-//             </p>
-//           </div>
-//         </div>
-//         <div className="flex gap-4 p-3 border-t border-gray-600 text-center">
-//           <button
-//             className="flex-1 bg-[#4F46E5] text-white py-2 rounded-xl hover:bg-blue-500 transition-colors duration-200"
-//             onClick={(e) => {
-//               e.stopPropagation(); // Prevent parent onClick
-//               handleEnquire(e); // Open the enquire popup
-//             }}
-//           >
-//             Enquire
-//           </button>
-
-//           <button
-//             className="flex items-center justify-center bg-green-500 text-white py-2 px-2 rounded-xl hover:bg-green-400 transition-colors duration-200"
-//             onClick={(e) => {
-//               e.stopPropagation(); // Prevent parent onClick
-//               handleWhatsApp(e); // Handle WhatsApp functionality
-//             }}
-//           >
-//             <FaWhatsapp className="w-6 h-6" />
-//           </button>
-//         </div>
-//       </div>
-
-//       {/* Show the popup form if isPopupOpen is true */}
-//       {isPopupOpen && <PopupForm onClose={closePopup} property={property} />}
-//     </div>
-//   );
-// };
-
-// export default ProjectCard;
+// code with skeleton in it . woeking code .
